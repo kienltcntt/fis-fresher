13f649bad7ef36ca010e257ed9cb0d3df3169efa
KienLT <68058309+kienltcntt@users.noreply.github.com>
2022-05-31 19:17:00 +0700

    Update excercise 2

---
+++ Sprint 1/MiniExercise/src/Form.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.util.List;
+
+public class Form {
+    List<Component> components;
+
+    public void addComponent(Component component) {
+        components.add(component);
+    }
+
+
+}
